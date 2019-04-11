23955ccc8dde875dd099a397854e39e8c13f973b
liuzidong <liuzidong@jd.com>
2019-04-11 18:02:05 +0800

    FIX init project

---
+++ src/main/java/net/liuzd/exceldatatoinsertsql/exceldatatoinsertsql/ExcelDataToInsertSqlApplication.java
@@ -0,0 +1,13 @@
+package net.liuzd.exceldatatoinsertsql.exceldatatoinsertsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDataToInsertSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDataToInsertSqlApplication.class, args);
+	}
+
+}
